34fbb5cb62a609512804bb4b0c2729b782fda1b0
Amirerfan Siamaki <amirerfan.siamaky@gmail.com>
2019-04-25 18:11:29 +0430

    complete login page check internet connection

--- app/src/main/java/com/example/shiraz_uni_app/Login/LoginActivity.java
+++ app/src/main/java/com/example/shiraz_uni_app/Login/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.shiraz_uni_app.Login;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.ColorSpace;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,11 +13,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.example.shiraz_uni_app.ForgetPassword.ForgetPassword;
 import com.example.shiraz_uni_app.MainActivity;
 import com.example.shiraz_uni_app.R;
+import com.example.shiraz_uni_app.Splash.SplashActivity;
 import com.orhanobut.hawk.Hawk;
 
 import java.util.Observable;
@@ -56,9 +61,11 @@ public class LoginActivity extends AppCompatActivity implements Observer, View.O
     public void update(Observable o, Object arg) {
         if (mModel.isValidLogin()){
             // TODO: 2019-04-17 : create an intent
+            Toast.makeText(this, "Internet account page", Toast.LENGTH_SHORT).show();
             Hawk.put("token" ,mModel.getToken());
         } else {
             // TODO: 2019-04-17 : unsuccessful login with wrong username or password
+
         }
     }
 
@@ -72,8 +79,22 @@ public class LoginActivity extends AppCompatActivity implements Observer, View.O
 
                 if(mConnectionStatus)
                     mModel.login(mUsername.getText().toString(), mPassword.getText().toString());
-                else
+                else {
                     // TODO: 2019-04-17 : show no internet dialog
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this); //the current class
+                    View dialogView = getLayoutInflater().inflate(R.layout.no_internet_connection_dialog, null);
+                    TextView close = dialogView.findViewById(R.id.close);
+                    builder.setView(dialogView);
+                    final AlertDialog dialog = builder.create();
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.show();
+                    close.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.cancel();
+                        }
+                    });
+                }
                 break;
             case (R.id.forgetPassword):
                 Intent intent = new Intent(LoginActivity.this , ForgetPassword.class);
