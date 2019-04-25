f0079d235771c580cdd51f1c90a12cb2a8d241f9
Bahare <bahare.836@gmail.com>
2019-04-26 00:18:43 +0430

    all events front

---
+++ app/src/main/java/com/example/shiraz_uni_app/AllEvents.java
@@ -0,0 +1,35 @@
+package com.example.shiraz_uni_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import java.util.ArrayList;
+
+
+public class AllEvents extends AppCompatActivity {
+    private RecyclerView recyclerView ;
+    private RecyclerView.Adapter adapter ;
+    private RecyclerView.LayoutManager layoutManager;
+    Button back ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_events);
+        back = findViewById(R.id.back);
+        ArrayList<Example> examples = new ArrayList<>();
+        examples.add(new Example("this is line 1 \nthis is line 2" ,"تاریخ برگزاری:1397/2/4" ) ) ;
+        examples.add(new Example("this is line 1 \nthis is line 2" ,"تاریخ برگزاری:1397/2/4" ) ) ;
+        examples.add(new Example("this is line 1 \nthis is line 2" ,"تاریخ برگزاری:1397/2/4" ) ) ;
+        examples.add(new Example("this is line 1 \nthis is line 2" ,"تاریخ برگزاری:1397/2/4" ) ) ;
+        examples.add(new Example("this is line 1 \nthis is line 2" ,"تاریخ برگزاری:1397/2/4" ) ) ;
+
+        recyclerView = findViewById(R.id.all_events);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new ExampleAdapter(examples);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/shiraz_uni_app/ExampleAdapter.java
@@ -0,0 +1,46 @@
+package com.example.shiraz_uni_app;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<Example> mExample ;
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder{
+        public TextView mycontext ;
+        public TextView mydate ;
+        public ExampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mycontext = itemView.findViewById(R.id.context);
+            mydate = itemView.findViewById(R.id.date);
+
+        }
+    }
+    public ExampleAdapter (ArrayList<Example> exampleList){
+        mExample = exampleList ;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_event_item, parent,false);
+        ExampleViewHolder evh = new ExampleViewHolder(v);
+        return  evh ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int i) {
+        Example currentItem = mExample.get(i);
+        exampleViewHolder.mycontext.setText(currentItem.getContext());
+        exampleViewHolder.mydate.setText(currentItem.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExample.size();
+    }
+}
