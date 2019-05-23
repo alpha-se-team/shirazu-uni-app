cd0a55e3bcd9afddea52e20cd0c5c1680892b0ea
MohammadReza Alaei <mohammadreza98alaii@gmail.com>
2019-05-23 16:29:51 +0430

    Add fav button UI (#22)
    Add favorite button
    Minor UI refactor

--- app/src/main/java/com/example/shiraz_uni_app/Event/SingleEvent.java
+++ app/src/main/java/com/example/shiraz_uni_app/Event/SingleEvent.java
@@ -1,21 +1,45 @@
 package com.example.shiraz_uni_app.Event;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.ablanco.zoomy.Zoomy;
 import com.example.shiraz_uni_app.R;
 
 public class SingleEvent extends AppCompatActivity {
+
+    boolean add_state = false; // add or remove event from favorites ... default= not added yet
+    ImageButton add_remove;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_event);
+
+        add_remove = (ImageButton) findViewById(R.id.add_remove);
+
         /*Zoom Feature*/
         ImageView event_view = (ImageView) findViewById(R.id.event_pic);
         // event_view.setImageResource(R.drawable.event_pic);
         Zoomy.Builder builder = new Zoomy.Builder(this).target(event_view);
         builder.register();
+
+    }
+
+    public void addFavorite(View view){
+        if(!add_state){
+            add_state = !add_state;
+            add_remove.setImageResource(R.drawable.add_animation);
+            ((AnimatedVectorDrawable) add_remove.getDrawable()).start();
+        }
+        else if (add_state) {
+            add_state = !add_state;
+            add_remove.setImageResource(R.drawable.remove_animation);
+            ((AnimatedVectorDrawable) add_remove.getDrawable()).start();
+        }
     }
 }
