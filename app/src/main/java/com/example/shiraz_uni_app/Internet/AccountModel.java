54e4060f7a7e56591aed56423b6e8dc0a135594d
shirin <shirinmhb98@gmail.com>
2019-05-24 14:49:20 +0430

    test

--- app/src/main/java/com/example/shiraz_uni_app/Internet/AccountModel.java
+++ app/src/main/java/com/example/shiraz_uni_app/Internet/AccountModel.java
@@ -1,7 +1,6 @@
 package com.example.shiraz_uni_app.Internet;
 
 import android.util.Log;
-import android.widget.TextView;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
