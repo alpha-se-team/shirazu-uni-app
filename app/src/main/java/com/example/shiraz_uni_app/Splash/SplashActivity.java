078f79d69bc5a46158e058beeb0e242d85e8bd89
shirin <shirinmhb98@gmail.com>
2019-06-05 21:47:15 +0430

    resolve conflict in splashActivity

--- app/src/main/java/com/example/shiraz_uni_app/Splash/SplashActivity.java
+++ app/src/main/java/com/example/shiraz_uni_app/Splash/SplashActivity.java
@@ -59,7 +59,7 @@ public class SplashActivity extends Activity implements Observer {
         Log.i("shirin" , "update called");
         if (mSplashModel.ismSuccess()) {
             Log.i("shirin" , "update1");
-            intent = new Intent(SplashActivity.this, EventsActivity.class);
+            intent = new Intent(SplashActivity.this, AccountActivity.class);
         } else {
             Log.i("shirin" , "update2");
             intent = new Intent(SplashActivity.this, LoginActivity.class);
